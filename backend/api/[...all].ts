13d4115840f03ea3ba1abe2b17066a7fcac4479a
fahad17 <fahadra96@gmail.com>
2025-12-04 00:44:38 +0700

    feat: add serverless-http integration and update vercel configuration

---
+++ backend/api/[...all].ts
@@ -0,0 +1,8 @@
+import serverless from 'serverless-http';
+import app from '../src/app';
+
+const handler = serverless(app);
+
+export default function (req: any, res: any) {
+  return handler(req, res);
+}
